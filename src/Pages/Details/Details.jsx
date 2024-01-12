b017be3c17b16a458d5ade8dca9832b7e6031bc9
Aurnab <zubayeraurnab@gmail.com>
2024-01-12 23:21:32 +0600

    Minor update on details page

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -5,9 +5,9 @@ const Details = () => {
     const customer = useLoaderData()
     console.log(customer)
     return (
-        <div className="mt-10 bg-base-200 p-4 w-4/5 mx-auto rounded-xl py-8">
+        <div className="mt-10 bg-base-200 p-4 md:w-4/5 mx-auto rounded-xl py-8">
             <h1 className="text-2xl font-bold text-center">{customer[0].name} <span></span></h1>
-            <div>
+            <div className="md:flex justify-between px-10 space-y-4">
                 <div className="space-y-5">
                     <p className="font-medium ">Package : <span className="font-normal">{customer[0].subscription} tk</span></p>
                     <p className="font-medium">User_Name :  <span className="font-normal">{customer[0].userName}</span></p>
@@ -16,7 +16,8 @@ const Details = () => {
                     <p className="font-medium">JoiningDate :  <span className="font-normal">{customer[0].joiningDate}</span></p>
                 </div>
                 <div>
-
+                    <p className="font-medium ">Status: <span className={customer[0].status ? "bg-green-300 px-4 rounded-full text-w font-bold" : "bg-red-200 px-4 rounded-full text-w font-bold"}>{customer[0].status ? customer[0].status : "unpaid"}</span></p>
+                    
                 </div>
 
             </div>
