8f362811d202085851efbd7bc107274a52d2cd39
Aurnab <zubayeraurnab@gmail.com>
2024-01-13 14:14:03 +0600

    Month picker added on detail Page

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,12 +1,19 @@
 import { useLoaderData } from "react-router-dom";
-
+import { useState } from 'react';
+import { MonthPicker, MonthInput } from 'react-lite-month-picker';
 
 const Details = () => {
     const customer = useLoaderData()
-    console.log(customer)
+    // console.log(customer)
+    const currentDate = new Date();
+    const [selectedMonthData, setSelectedMonthData] = useState({
+        month: currentDate.getMonth() + 1,
+        year: currentDate.getFullYear(),
+    });
+    const [isPickerOpen, setIsPickerOpen] = useState(false);
     return (
-        <div className="mt-10 bg-base-200 p-4 md:w-4/5 mx-auto rounded-xl py-8">
-            <h1 className="text-2xl font-bold text-center">{customer[0].name} <span></span></h1>
+        <div className="mt-10 bg-base-200 p-4 md:w-1/2 mx-auto rounded-xl py-8">
+            <h1 className="text-2xl font-bold text-center mb-10">{customer[0].name}</h1>
             <div className="md:flex justify-between px-10 space-y-4">
                 <div className="space-y-5">
                     <p className="font-medium ">Package : <span className="font-normal">{customer[0].subscription} tk</span></p>
@@ -17,7 +24,31 @@ const Details = () => {
                 </div>
                 <div>
                     <p className="font-medium ">Status: <span className={customer[0].status ? "bg-green-300 px-4 rounded-full text-w font-bold" : "bg-red-200 px-4 rounded-full text-w font-bold"}>{customer[0].status ? customer[0].status : "unpaid"}</span></p>
-                    
+                    {
+                        customer[0].status === "paid" ?
+                         ""
+                            :
+                            <div className="mt-5">
+                                <h1 className="mb-2">Make Payment</h1>
+                                <div >
+                                    <MonthInput
+                                        selected={selectedMonthData}
+                                        setShowMonthPicker={setIsPickerOpen}
+                                        showMonthPicker={isPickerOpen}
+                                        size="small"
+                                    />
+                                    {isPickerOpen ? (
+                                        <MonthPicker
+                                            setIsOpen={setIsPickerOpen}
+                                            selected={selectedMonthData}
+                                            onChange={setSelectedMonthData}
+                                            size="small"
+                                        />
+                                    ) : null}
+                                </div>
+                            </div>
+                    }
+
                 </div>
 
             </div>
